38d0c0e21367e40d2ceab0ec1bb431a8575d6556
harish <harishtamilan05@gmail.com>
2021-08-01 08:51:23 +0530

    Food Order First Commit

---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './MealItem.module.css';
+import Card from '../../UI/Card';
+import MealItemForm from './MealItemForm';
+
+const MealItem = (props)=>{
+    return(
+        <li className={classes.meal}>
+            <div>
+                <h3>{props.meal.name}</h3>
+                <div className={classes.description}>{props.meal.description}</div>
+                <div className={classes.price}>${props.meal.price}</div>
+            </div>
+            <div>
+                <MealItemForm id={props.id}/>
+            </div>
+        </li>
+    );
+}
+
+export default MealItem;
